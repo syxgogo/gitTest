8bf97c11b0cc9d9806028233b8df84c8024af822
syxgogo <83436548+syxgogo@users.noreply.github.com>
2022-02-24 19:33:55 +0800

    再次修改

--- src/main/java/com/example/gittest/GitTestApplication.java
+++ src/main/java/com/example/gittest/GitTestApplication.java
@@ -1,13 +1,20 @@
 package com.example.gittest;
 
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class GitTestApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(GitTestApplication.class, args);
+        ConfigurableApplicationContext run = SpringApplication.run(GitTestApplication.class, args);
+
+        ConfigurableListableBeanFactory beanFactory = run.getBeanFactory();
+        System.out.println(beanFactory);
+
+
     }
 
 }
